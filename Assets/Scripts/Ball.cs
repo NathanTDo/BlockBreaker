50da7a4823b59602c748c2b9ca27174742a1de2d
NathanTDo <nathantdo04@gmail.com>
2020-07-12 17:23:39 -0700

    Added all assets and files

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    // config params
+    [SerializeField] Paddle paddle1 = null;
+
+    // state
+    Vector2 paddleToBallDistance;
+
+    // velocity params
+    [SerializeField] float horVelocity = 5;
+    [SerializeField] float vertVelocity = 5;
+
+    bool hasStarted = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallDistance = (transform.position - paddle1.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(horVelocity, vertVelocity);
+        }
+    }
+
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallDistance;
+    }
+}
